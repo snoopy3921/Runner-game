6afe2ff68a250f1c4495e4b84853e08c85a75193
snoopy3921 <giahuy392001@gmail.com>
2025-04-30 20:22:51 +0300

    remove build files

--- build_Runner-game/button.d
+++
@@ -1,4 +0,0 @@
-build_Runner-game/button.o: sources/driver/button/button.c \
- sources/driver/button/button.h \
- /home/giahuy/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
- /home/giahuy/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdint-gcc.h
--- build_Runner-game/glcdfont.d
+++
@@ -1,2 +0,0 @@
-build_Runner-game/glcdfont.o: \
- sources/driver/Adafruit_ssd1306syp/glcdfont.cpp
